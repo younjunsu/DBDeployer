7279c85d1f86522d714ff4b6f1185f9ded55bbc7
younjunsu <junsu_youn@naver.com>
2022-01-27 13:43:27 +0900

    Create auto_install_sciprts.sh

---
+++ auto_install_sciprts.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+
+###### CONFIGURATION
+## Control file Mutiplex 2
+## Value Y or N
+CONTROL_FILE_MULTI=Y
+
+## 
+###### PATH
+TIBERO_ENGINE_PATH=/tibero/tibero6
+TIBERO_DATA_PATH=/data
+TIBERO_ARCHIVE_PATH=/archive
+
+
+##### Configration Check Process
+
+
+##### TIBERO ENGINE PATH Created directory
+mkdir -p $TIBERO_ENGINE_PATH/tbctl1
+mkdir -p $TIBERO_ENGINE_PATH/tbdata
+mkdir -p $TIBERO_DATA_PATH/tbctl1
+mkdir -p $TIBERO_DATA_PATH/tbctl1
+
+
+
+
