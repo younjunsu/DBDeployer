6c13b34fc04f74412960e0f0555a23dfe7d50014
younjunsu <junsu_youn@naver.com>
2022-02-17 11:26:41 +0900

    Update tb_main_install.sh

--- TB_INSTALL_SHELL/tb_main_install.sh
+++ TB_INSTALL_SHELL/tb_main_install.sh
@@ -10,114 +10,156 @@ TB_RESULT_FILE="./tb_files"
 
 ############################################################
 #
-# Function lists
+# lists
 ############################################################
 # Press key
-function FN_PRINT_PRESS(){
+FN_PRINT_PRESS(){
     echo "Press Enter Key to continue..."
 }
 
 # Type Select
-function FN_PRINT_TYPE(){
-    echo "############################"
+FN_PRINT_TYPE(){
+    echo "---------------------------"
     echo "# TIBERO TYPE"
     echo "# 1. SINGLE"
     echo "# 2. TSC"
     echo "# 3. TAC"
-    echo "# 4. PROSYNC"
-    echo "# 5. TAS"
-    echo "############################"
-    echo -n "Choose the Number : "
-    
-    read INPUT_TYPE
-    export INPUT_TYPE=$INPUT_TYPE
-    
-    # Type Select exception out
-    if [ $INPUT_TYPE != "1" ] && [ $INPUT_TYPE != "2" ] && [ $INPUT_TYPE != "3" ] && [ $INPUT_TYPE != "4" ] && [ $INPUT_TYPE != "5" ]
-    then
+    echo "# 4. TAS"
+    echo "# m. Mnaul menu screen"
+    echo "# press other key to quit"
+    echo "---------------------------"
+    printf "Choose the Number : "
+    read PRINT_TYPE_INPUT_TYPE
+    echo
+    echo
+    echo "---------------------------"
+    case $PRINT_TYPE_INPUT_TYPE in
+    1)
+        echo "# TYPE : SINGLE"
+        export PRINT_TYPE_INPUT_TYPE=$PRINT_TYPE_INPUT_TYPE
+    ;;
+    2)
+        echo "# TYPE : TSC"
+        export PRINT_TYPE_INPUT_TYPE=$PRINT_TYPE_INPUT_TYPE
+    ;;
+    3)
+        echo "# TYPE : TAC"
+       export PRINT_TYPE_INPUT_TYPE=$PRINT_TYPE_INPUT_TYPE
+    ;;
+    4)
+        echo "# TYPE : TAS"
+        export PRINT_TYPE_INPUT_TYPE=$PRINT_TYPE_INPUT_TYPE
+    ;;
+    m)
+        FN_MANUAL_MENU
+    ;;
+    *)
+        clear
+        echo "tb_install_shell.sh quit."
         exit 1
-    fi
+    ;;
+    esac
 }
 
-function FN_PRINT_TYPE_DETAIL(){
-    TYPE=$INPUT_TYPE
+FN_PRINT_TYPE_DETAIL(){
+    PRINT_TYPE_DETAIL_TYPE=$PRINT_TYPE_INPUT_TYPE
 
     if [ $TYPE == "2" ]
     then
-        echo "############################"
+        echo "---------------------------"
         echo "# TIBERO TSC DETAIL TYPE"
         echo "# 1. TSC Primary"
         echo "# 2. TSC Standby"
         echo "# 3. TSC Observer"
-        echo "############################"
-        echo -n "Choose the Number : "
+        echo "# m. Mnaul menu screen"
+        echo "# press other key to quit"
+        echo "---------------------------"
+        printf "Choose the Number : "
 
-        read INPUT_TYPE_DETAIL
-        export INPUT_TYPE_DETAIL=$INPUT_TYPE_DETAIL
+        read PRINT_TYPE_DETAIL_TYPE
+        export PRINT_TYPE_DETAIL_TYPE=$PRINT_TYPE_DETAIL_TYPE
     elif [ $TYPE == "3" ]
     then
-        echo "############################"
+        echo "---------------------------"
         echo "# TIBERO TAC DETAIL TYPE"
         echo "# 1. TAC THREAD 0"
         echo "# 2. TAC THREAD 1"
-        echo "############################"
-        echo -n "Choose the Number : "
+        echo "# m. Mnaul menu screen"
+        echo "---------------------------"
+        printf "Choose the Number : "
 
-        read INPUT_TYPE_DETAIL
-        export INPUT_TYPE_DETAIL=$INPUT_TYPE_DETAIL        
+        read PRINT_TYPE_DETAIL_TYPE
+        export PRINT_TYPE_DETAIL_TYPE=$PRINT_TYPE_DETAIL_TYPE        
     else
         exit 1
     fi
-
-
 }
 
 # Automatic menu
-function FN_AUTOMATIC_MENU(){
+FN_AUTOMATIC_MENU(){
     echo "FN_AUTOMATIC_MENU"
+}
+FN_CALL_MANUAL_MENU(){
+    FN_MANUAL_MENU
 }
 
 # Manual menu
-function FN_MANUAL_MENU(){
+FN_MANUAL_MENU(){
     clear
     echo " -----------------------------------------------------------------------------------"
-    echo "  1.TIBERO USER CREATE                    |  2.TIBERO User Profile                  "
+    echo "  1.TIBERO USER CREATE                      |  2.TIBERO User Profile                  "
     echo " ---------------------------------------- + ----------------------------------------"
-    echo "  11 - MANUAL                             |  21 - MANUAL                            "
-    echo "                                          |                                         "
+    echo "  # 11 - MANUAL                             |  # 21 - MANUAL                            "
+    echo "                                            |                                         "
     echo " -----------------------------------------------------------------------------------"
-    echo "  3.TIBERO Directory                      |  4.TIBERO TIP                           "
+    echo "  3.TIBERO Directory                        |  4.TIBERO TIP                           "
     echo " ---------------------------------------- + ----------------------------------------"
-    echo "  31 - MANUAL                             |  41 - MANUAL                            "
-    echo "                                          |                                         "
+    echo "  # 31 - MANUAL                             |  # 41 - MANUAL                            "
+    echo "                                            |                                         "
     echo " -----------------------------------------------------------------------------------"
-    echo "  5.TIBERO CREATE DATABASE                |  6.TIBERO CM Resource                   "
+    echo "  5.TIBERO CREATE DATABASE                  |  6.TIBERO CM Resource                   "
     echo " ---------------------------------------- + ----------------------------------------"
-    echo "  51 - MANUAL                             |  61 - MANUAL                            "
-    echo "                                          |                                         "
+    echo "  # 51 - MANUAL                             |  # 61 - MANUAL                            "
+    echo "                                            |                                         "
     echo " -----------------------------------------------------------------------------------"
     echo "  c  - Clear terminal                                                               "
     echo "  r  - retry main menu                                                              "
-    echo "  Other key - exit                                                                  "
+    echo "  press other key to quit                                                                  "
     echo " -----------------------------------------------------------------------------------"
-    echo -n " Choose the Number or Command : "    
+    printf " Choose the Number or Command : "    
 
     read INPUT_NUMBER
     case $INPUT_NUMBER in
     11)
         clear
-        echo -n "# GROUP NAME : "
+        echo "-----------------------------------------------------------------"
+        echo "# /etc/group"
+        cat /etc/group |tail -n 5
+        echo "-----------------------------------------------------------------"
+        echo
+        printf "# GROUP NAME : "
         read INPUT_GROUPNM
-        echo -n "# GROUP GID : "
+        printf "# GROUP GID : "
         read INPUT_GROUPGID
-        echo -n "# USER NAME : "
+        echo
+        echo "-----------------------------------------------------------------"
+        echo "# /etc/passwd"
+        cat /etc/passwd |tail -n 5
+        echo "-----------------------------------------------------------------"
+        echo
+        printf "# USER NAME : "
         read INPUT_USERNM
-        echo -n "# USER UID : "
+        printf "# USER UID : "
         read INPUT_USERUID
+        printf "# USER HOME PATH : "
+        read INPUT_HOMEPATH
+        echo
+        echo
+        clear
         if [ -z $INPUT_USERUID ] || [ -z $INPUT_GROUPGID ]
         then
-            echo -n "# USER HOME PATH : "
-            read INPUT_HOMEPATH
+            echo "-----------------------------------------------------------------"
+            echo "# OS USER/GROUP Result"
             echo "-----------------------------------------------------------------"
             echo "# LINUX | HP-UX | Solaris"
             echo
@@ -126,16 +168,16 @@ function FN_MANUAL_MENU(){
             echo
             echo "-----------------------------------------------------------------"
             echo "# AIX "
-            echo 
-            echo
+            echo "mkgroup $INPUT_GROUPNM"
+            echo "useradd $INPUT_USERNM -d $INPUT_HOMEPATH -g $INPUT_GROUPNM"
             echo "-----------------------------------------------------------------"
             echo
             echo
             FN_PRINT_PRESS
             read PRESS_KEY        
         else
-            echo -n "# USER HOME PATH : "
-            read INPUT_HOMEPATH
+            echo "-----------------------------------------------------------------"
+            echo "# USER/GROUP Result"
             echo "-----------------------------------------------------------------"
             echo "# LINUX | HP-UX | Solaris"
             echo
@@ -144,29 +186,27 @@ function FN_MANUAL_MENU(){
             echo
             echo "-----------------------------------------------------------------"
             echo "# AIX "
-            echo 
-            echo
+            echo "mkgroup $INPUT_GROUPNM -A id=$INPUT_GROUPGID"
+            echo "useradd $INPUT_USERNM -d $INPUT_HOMEPATH -g $INPUT_GROUPNM -u $INPUT_USERUID"
             echo "-----------------------------------------------------------------"
             echo
             echo
             FN_PRINT_PRESS
             read PRESS_KEY
         fi
-
+        FN_CALL_MANUAL_MENU
     ;;
     21)
         clear
         FN_PRINT_TYPE      
-
-        echo
-        echo "############################"
-        echo -n "# USER NAME : "
+        echo "---------------------------"
+        printf "# USER NAME : "
         read INPUT_USER
-        echo -n "# DB NAME : "
+        printf "# DB NAME : "
         read INPUT_DBNM
-        echo -n "# CM_SID NAME : "
+        printf "# CM_SID NAME : "
         read INPUT_CMNM
-        echo -n "# TB_HOME PATH : "
+        printf "# TB_HOME PATH : "
         read INPUT_TBHOME
         echo
         USER_HOME_PATH=`echo $USER_HOME_PATH |sed 's/=/ /g' |awk '{print $NF}'`
@@ -233,17 +273,22 @@ function FN_MANUAL_MENU(){
         FN_CHOICE_MENU
     ;;
     *)
+        clear
+        echo "tb_install_shell.sh quit."
         exit 1
     ;;
     esac
 }
 
 # Main function
-function FN_CHOICE_MENU(){
+FN_CHOICE_MENU(){
     echo "* TIBERO Installation Shell Script"
     echo "* This script is for TmaxTibero "Daejeon Office" only."
-    echo "1 AUTO SCRIPT"
-    echo "2 MANUAL SCRIPT"
+    echo "-----------------------------------------------------------------"
+    echo "# 1 AUTO SCRIPT"
+    echo "# 2 MANUAL SCRIPT"
+    echo "# press other key to quit"
+    echo "-----------------------------------------------------------------"
     read INPUT_NUMBER
     case $INPUT_NUMBER in 
     1)
@@ -257,6 +302,8 @@ function FN_CHOICE_MENU(){
         done
     ;;
     *)
+        clear
+        echo "tb_install_shell.sh quit."
         exit 1
     ;;
     esac
@@ -264,7 +311,7 @@ function FN_CHOICE_MENU(){
 
 ############################################################
 #
-# Main function running
+# Main running
 ############################################################
 clear
-FN_CHOICE_MENU
+FN_CHOICE_MENU 2>/dev/null
