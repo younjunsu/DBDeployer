447ef2dc8224f1bc82095bdc32f03831bf11d979
younjunsu <junsu_youn@naver.com>
2022-02-22 00:59:44 +0900

    update

--- TB_INSTALL_SHELL/tbis_ini.sh
+++ TB_INSTALL_SHELL/tbis_ini.sh
@@ -13,7 +13,7 @@ INPUT_INI_TYPE=$1
 INPUT_TB_TYPE=$2
 INPUT_TB_DETAIL_TYPE=$3
 #INPUT_TB_HOME=$4
-#INPUT_TB_SID=$5
+#INPUT_DB_NAME=$5
 #INPUT_TB_DB_NAME=$6
 
 ############################# Profile
@@ -42,34 +42,30 @@ FN_USER_PROFILE(){
     echo "#### JAVA"
     echo "#export JAVA_HOME="
     echo ""
-    echo "#### SINGLE"
-    echo "export TB_SID=$INPUT_TB_SID"
-    echo "export TB_HOME=$INPUT_TB_HOME"
-    echo "export TB_PROF_DIR=\$TB_HOME/bin/prof"
-    echo "export LD_LIBRARY_PATH=\$TB_HOME/lib:$TB_HOME/client/lib"
-    echo "export LIBPATH=\$LD_LIBRARY_PATH"
-    echo "export SHLIB_PATH=\$LD_LIBRARY_PATH"
-    echo "export PATH\$PATH:\$TB_HOME/bin:\$TB_HOME/client/bin:\$JAVA_HOME/bin"
-    echo ""
-    echo ""
     if [ $INPUT_TB_TYPE == "1" ]
     then
-        2>/dev/null
+        echo "#### SINGLE"
+        echo "export TB_SID=`echo $INPUT_DB_NAME`"
+        echo "export TB_HOME=$INPUT_TB_HOME_PATH"
     elif [ $INPUT_TB_TYPE == "2" ]
     then
         echo "#### TSC (Tibero Standby Cluster)"
         if [ $INPUT_TB_DETAIL_TYPE == "1" ]
         then
+            echo "export TB_SID=`echo $INPUT_DB_NAME`_p"
+            echo "export TB_HOME=$INPUT_TB_HOME_PATH"
             echo "export CM_SID=primary"
-            echo "export CM_HOME=$INPUT_TB_HOME"
+            echo "export CM_HOME=\$TB_HOME"
         elif [ $INPUT_TB_DETAIL_TYPE == "2" ]
         then
+            echo "export TB_SID=`echo $INPUT_DB_NAME`_s"
+            echo "export TB_HOME=$INPUT_TB_HOME_PATH"
             echo "export CM_SID=standby"
-            echo "export CM_HOME=$INPUT_TB_HOME"
+            echo "export CM_HOME=\$TB_HOME"
         elif [ $INPUT_TB_DETAIL_TYPE == "3" ]
         then
             echo "export CM_SID=observer"
-            echo "export CM_HOME=$INPUT_TB_HOME"
+            echo "export CM_HOME=\$TB_HOME"
         else
             2>/dev/null
         fi
@@ -78,15 +74,26 @@ FN_USER_PROFILE(){
         echo "#### TAC (Tibero Active Cluster)"
         if [ $INPUT_TB_DETAIL_TYPE == "1" ]
         then
+            echo "export TB_SID=`echo $INPUT_DB_NAME`0"
+            echo "export TB_HOME=$INPUT_TB_HOME_PATH"
             echo "export CM_SID=cm0"
-            echo "export CM_HOME=$INPUT_TB_HOME"
+            echo "export CM_HOME=\$TB_HOME"
         elif [ $INPUT_TB_DETAIL_TYPE == "2" ]
         then
+            echo "export TB_SID=`echo $INPUT_DB_NAME`1"
+            echo "export TB_HOME=$INPUT_TB_HOME_PATH"
             echo "export CM_SID=cm1"
-            echo "export CM_HOME=$INPUT_TB_HOME"
+            echo "export CM_HOME=\$TB_HOME"
         else
             2>dev/null
         fi
+        echo "### Genernal"
+        echo "export TB_PROF_DIR=\$TB_HOME/bin/prof"
+        echo "export LD_LIBRARY_PATH=\$TB_HOME/lib:$TB_HOME/client/lib"
+        echo "export LIBPATH=\$LD_LIBRARY_PATH"
+        echo "export SHLIB_PATH=\$LD_LIBRARY_PATH"
+        echo "export PATH\$PATH:\$TB_HOME/bin:\$TB_HOME/client/bin:\$JAVA_HOME/bin"
+        echo ""
     elif [ $InPUT_TB_TYPE == "4" ]
     then
         2>/dev/null
@@ -102,11 +109,11 @@ FN_USER_PROFILE(){
 FN_TB_SID_TIP(){
     echo "############ TIBERO genernal"
     echo "#### Must"
-    echo "DB_NAME="
+    echo "DB_NAME=$INPUT_DB_NAME"
     echo "LISTENER_PORT=8629"
     echo "CONTROL_FILES=$INPUT_CONTROL_FILES_PATH1/tbctl1/c1.ctl,$INPUT_CONTROL_FILES_PATH2/tbctl2/c2.ctl"
     echo "DB_CREATE_FILE_DEST=$INPUT_DB_CREATE_FILE_DEST/tbdata"
-    echo "LOG_ARCHIVE_DEST=$INPUT_LOG_ARCHIVE_DEST"
+    echo "LOG_ARCHIVE_DEST=$INPUT_LOG_ARCHIVE_DEST/tbarch"
     echo "MAX_SESSION_COUNT=100"
     echo "TOTAL_SHM_SIZE=2G"
     echo "MEMORY_TARGET=4G"
@@ -150,13 +157,13 @@ FN_TB_SID_TIP(){
         if [ $INPUT_TB_DETAIL_TYPE == "1" ]
         then
             echo "## Primary"
-            echo "LOG_REPLICATION_DEST_1=\"<Standby IP>:8633 LGWR ASYNC\""
-            echo "LOCAL_CLUSTER_ADDR=<Primary IP>"
+            echo "LOG_REPLICATION_DEST_1=\"$INPUT_NODE2_INTER_IP:8633 LGWR ASYNC\""
+            echo "LOCAL_CLUSTER_ADDR=$INPUT_NODE1_INTER_IP"
         elif [ $INPUT_TB_DETAIL_TYPE == "2" ]
         then
             echo "## standby"
-            echo "LOG_REPLICATION_DEST_1=\"<Primary IP>:8633 LGWR ASYNC\""
-            echo "LOCAL_CLUSTER_ADDR=<Standby IP>"
+            echo "LOG_REPLICATION_DEST_1=\"$INPUT_NODE1_INTER_IP:8633 LGWR ASYNC\""
+            echo "LOCAL_CLUSTER_ADDR=$INPUT_NODE2_INTER_IP"
         elif [ $INPUT_TB_DETAIL_TYPE == "3" ]
         then
             2>/dev/null
@@ -175,7 +182,7 @@ FN_TB_SID_TIP(){
             echo "UNDO_TABLESPACE=UNDO0"
             echo "CM_NAME=cm0"
             echo "CM_PORT=28629"
-            echo "LOCAL_CLUSTER_ADDR=<localhost IP>"
+            echo "LOCAL_CLUSTER_ADDR=$INPUT_NODE1_INTER_IP"
             echo "LOCAL_CLUSTER_PORT=18629"
         elif [ $INPUT_TB_DETAIL_TYPE == "2" ]
         then
@@ -184,7 +191,7 @@ FN_TB_SID_TIP(){
             echo "UNDO_TABLESPACE=UNDO1"
             echo "CM_NAME=cm1"
             echo "CM_PORT=28629"
-            echo "LOCAL_CLUSTER_ADDR=<localhost IP>"
+            echo "LOCAL_CLUSTER_ADDR=$INPUT_NODE2_INTER_IP"
             echo "LOCAL_CLUSTER_PORT=18629"
         fi
         echo ""
