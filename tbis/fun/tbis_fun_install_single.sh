fa33dec6e94a0aa5332a9053007c76dd22afd0a6
younjunsu <junsu_youn@naver.com>
2022-09-16 12:03:45 +0900

    update check

--- tbis/fun/tbis_fun_install_single.sh
+++ tbis/fun/tbis_fun_install_single.sh
@@ -1,11 +1,11 @@
 # tibero profile configuration apply
-. $tbis_current_path/cfg/tbis_cfg_profile.sh output
+. $tbis_current_path/cfg/tbis_cfg_profile.sh apply
 
 # tibero db tip configuration apply
-. $tbis_current_path/cfg/tbis_cfg_db.sh output
+. $tbis_current_path/cfg/tbis_cfg_db.sh apply
 
 # tibero db tbdsn.tbr configuration apply
-. $tbis_current_path/cfg/tbis_cfg_tbdsn.sh output
+. $tbis_current_path/cfg/tbis_cfg_tbdsn.sh apply
 
 # change owner
 . $tbis_current_path/fun/tbis_fun_action.sh chang_owner
